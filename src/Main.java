194b78926a7c56f6a091183d02ba0fd953e7fb04
mariaangelica <ma.kcond@gmail.com>
2022-10-03 15:41:04 -0300

    SmartTv, métodos

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume: " + smartTv.volume);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.diminuirCanal();
+        smartTv.diminuirCanal();
+
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+    }
+}
